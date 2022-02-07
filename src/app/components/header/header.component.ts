627b7f9d8506476fc37e556e5d0f55e0a9f319a7
salmenhsairi <hsairisalmen.sh@gmail.com>
2022-02-07 20:58:57 +0100

    task tracker almost completed

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  //1:47:40 / 2:01:31 To Continue
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+}
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TASKS } from '../mock-tasks';
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+// provided into the applciation level
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http:HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task) : Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    // Since we are sending data in the http request, we must include a http header as well
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
+}
